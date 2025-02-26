5557ade18002e6838dd8608d7d3d1e6a1281c5e9
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-02-27 00:39:48 +0200

    added category entity and her functionality

--- src/main/java/online/book/store/mapper/BookMapper.java
+++ src/main/java/online/book/store/mapper/BookMapper.java
@@ -1,9 +1,13 @@
 package online.book.store.mapper;
 
+import java.util.stream.Collectors;
 import online.book.store.config.MapperConfig;
 import online.book.store.dto.book.BookDto;
+import online.book.store.dto.book.BookDtoWithoutCategoryIds;
 import online.book.store.dto.book.CreateBookRequestDto;
 import online.book.store.model.Book;
+import online.book.store.model.Category;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
@@ -12,6 +16,15 @@ public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        bookDto.setCategoryIds(book.getCategories().stream()
+                .map(Category::getId)
+                .collect(Collectors.toSet()));
+    }
 
     void updateBookFromDto(CreateBookRequestDto requestDto, @MappingTarget Book existingBook);
 }
--- src/main/java/online/book/store/repository/book/BookRepository.java
+++ src/main/java/online/book/store/repository/book/BookRepository.java
@@ -1,8 +1,13 @@
 package online.book.store.repository.book;
 
+import java.util.List;
 import online.book.store.model.Book;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @EntityGraph(attributePaths = "categories")
+    List<Book> findAllByCategoriesId(Pageable pageable, Long categoryId);
 }
